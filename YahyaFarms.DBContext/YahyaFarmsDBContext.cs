fd1e9700806437e0515017bd6a87e57f5400a5d8
Wael Galal EL Deen <waelmg50@hotmail.com>
2024-07-11 10:45:53 +0300

    Add project files.

---
+++ YahyaFarms.DBContext/YahyaFarmsDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YahyaFarms.Models;
+
+namespace YahyaFarms.DBContext
+{
+    public class YahyaFarmsDBContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+    }
+}
---
+++ YahyaFarms.Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YahyaFarms.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("اسم الصنف:")]
+        public required string ItemName { get; set; }
+        [MaxLength(1000)]
+        [DisplayName("الوصف:")]
+        public string ItemDescription { get; set; } = string.Empty;
+        [Required]
+        [Column(nameof(CurrentPrice), TypeName = "decimal(8, 3)")]
+        [DisplayName("السعر:")]
+        public decimal CurrentPrice { get; set; }
+    }
+}
