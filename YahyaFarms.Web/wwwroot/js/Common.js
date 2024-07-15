26e11eee3e8af3cb88a923e009cd3987ff419c9f
Wael Galal EL Deen <waelmg50@hotmail.com>
2024-07-15 13:57:00 +0300

    Add delete image

--- YahyaFarms.Web/wwwroot/js/Common.js
+++ YahyaFarms.Web/wwwroot/js/Common.js
@@ -1,14 +1,14 @@
 ﻿function ShowToastr(type, message) {
     if (type === 'success')
-        toastr.success(message, "Yahya Farms", { timeOut: 2000, positionClass: "toast-top-left" });
+        toastr.success(message, "مزارع يحيى", { timeOut: 2000, positionClass: "toast-top-left" });
     else if (type === 'error')
-        toastr.error(message, "Yahya Farms", { timeOut: 2000, positionClass: "toast-top-left" });
+        toastr.error(message, "مزارع يحيى", { timeOut: 2000, positionClass: "toast-top-left" });
 }
 SwalConfirm = () => {
     return new Promise(resolve => {
         Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
+            title: 'هل أنت متأكد؟',
+            text: "لن يمكنك التراجع عن هذه العملية!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
